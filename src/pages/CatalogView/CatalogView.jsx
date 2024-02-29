47ee4e846c890e45490cd003404dfb2e11346cb4
Monica Stoyanova <monicastoyanova1@gmail.com>
2024-02-29 11:32:52 +0200

    Catalog table view and search bar

--- src/pages/CatalogView/CatalogView.jsx
+++ src/pages/CatalogView/CatalogView.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import DataGridDemo from "./CatalogTable";
+import SearchAppBar from "../../components/Searchbar/Searchbar";
 
 const CatalogView = () => {
   return (
     <>
       <Header />
+      <SearchAppBar />
       <DataGridDemo />
     </>
   );
